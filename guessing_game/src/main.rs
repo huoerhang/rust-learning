315c2a8d115182d16da27a87d958fce2f119d473
CV Developer <huohaihang@126.com>
2021-04-22 19:19:20 +0800

    guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,16 +5,14 @@ use std::io;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101);
-
-    let mut guess = String::new();
-
     loop {
+        let mut guess = String::new();
         println!("Please input your guess.");
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
+
         let guess: u32 = guess.trim().parse().expect("Please type a number!");
-        
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
